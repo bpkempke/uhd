7cabfc01673fdd4dc08d149aa8af5fff5a21269e
Ben Kempke <bpkempke@umich.edu>
2017-08-16 14:46:57 -0600

    branch to test always using the higher frequency LNA

--- host/lib/usrp/dboard/db_ubx.cpp
+++ host/lib/usrp/dboard/db_ubx.cpp
@@ -1017,8 +1017,8 @@ private:
         }
         else if ((freq >= 100*fMHz) && (freq < 500*fMHz))
         {
-            set_cpld_field(SEL_LNA1, 0);
-            set_cpld_field(SEL_LNA2, 1);
+            set_cpld_field(SEL_LNA1, 1);
+            set_cpld_field(SEL_LNA2, 0);
             set_cpld_field(RXLO1_FSEL3, 1);
             set_cpld_field(RXLO1_FSEL2, 0);
             set_cpld_field(RXLO1_FSEL1, 0);
@@ -1033,8 +1033,8 @@ private:
         }
         else if ((freq >= 500*fMHz) && (freq < 800*fMHz))
         {
-            set_cpld_field(SEL_LNA1, 0);
-            set_cpld_field(SEL_LNA2, 1);
+            set_cpld_field(SEL_LNA1, 1);
+            set_cpld_field(SEL_LNA2, 0);
             set_cpld_field(RXLO1_FSEL3, 0);
             set_cpld_field(RXLO1_FSEL2, 0);
             set_cpld_field(RXLO1_FSEL1, 1);
@@ -1045,8 +1045,8 @@ private:
         }
         else if ((freq >= 800*fMHz) && (freq < 1000*fMHz))
         {
-            set_cpld_field(SEL_LNA1, 0);
-            set_cpld_field(SEL_LNA2, 1);
+            set_cpld_field(SEL_LNA1, 1);
+            set_cpld_field(SEL_LNA2, 0);
             set_cpld_field(RXLO1_FSEL3, 0);
             set_cpld_field(RXLO1_FSEL2, 0);
             set_cpld_field(RXLO1_FSEL1, 1);
@@ -1057,8 +1057,8 @@ private:
         }
         else if ((freq >= 1000*fMHz) && (freq < 1500*fMHz))
         {
-            set_cpld_field(SEL_LNA1, 0);
-            set_cpld_field(SEL_LNA2, 1);
+            set_cpld_field(SEL_LNA1, 1);
+            set_cpld_field(SEL_LNA2, 0);
             set_cpld_field(RXLO1_FSEL3, 0);
             set_cpld_field(RXLO1_FSEL2, 1);
             set_cpld_field(RXLO1_FSEL1, 0);
